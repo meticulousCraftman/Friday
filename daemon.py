678ee1a6f981e8fc9c4411deee9ccc1246debcef
Apoorva Singh <apoorvasingh157@gmail.com>
2018-04-20 16:26:01 +0530

    Listens to the commands from Google Assistant and displays a small notification.

--- daemon.py
+++ daemon.py
@@ -8,24 +8,27 @@ Working:
 """
 if __name__ == '__main__':
     from logger import *
+    from components.notifier import notify
     from commander import get_command
-    from content_retriever import get_content
-    from content_provider import get_download_url
-    from downloader import start_download
+    # from content_retriever import get_content
+    # from content_provider import get_download_url
+    # from downloader import start_download
 
     while True:
         try:
             cmd = get_command()
+
             logging.info(f'Got command {repr(cmd)}')
+            notify(f'Received a command from Google Assistant: {cmd}')
 
-            content = get_content(cmd)
-            logging.info(f'Got content {repr(content)}')
-
-            magnet = get_download_url(content)
-            logging.info(f'Got download url {magnet}')
-
-            start_download(magnet)
-            logging.info('Started download')
+            # content = get_content(cmd)
+            # logging.info(f'Got content {repr(content)}')
+            #
+            # magnet = get_download_url(content)
+            # logging.info(f'Got download url {magnet}')
+            #
+            # start_download(magnet)
+            # logging.info('Started download')
         except KeyboardInterrupt:
             logging.info('exiting!')
             break
