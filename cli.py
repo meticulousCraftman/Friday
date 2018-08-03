8c622819f98de0ecfabfc65c8f931030bae7ae76
Apoorva Singh <apoorvasingh157@gmail.com>
2018-08-03 21:34:44 +0530

    Prompt working. Commands are not working right now.

--- cli.py
+++ cli.py
@@ -5,8 +5,11 @@ Talk to Friday using command line interface
 from service_handler import serve
 
 print("<==== Friday CLI ====>")
-
+CONTEXT = {"iteration":1}
 while True:
-    CONTEXT = {}
+    if CONTEXT['iteration'] == 1:
+        print("[*] Hello !!!")
+
     msg = input("<*> ")
     serve(msg, CONTEXT)
+    CONTEXT['iteration'] += 1
